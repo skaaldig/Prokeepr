c7145d3da49fe1d50348447a34822b5560179ec1
[Ryan Staley] <rericstaley@gmail.com>
2019-03-21 15:20:20 -0400

    added basic views and created product list view template/styles

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("users/", include("borrowing.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("products", include("products.urls")),
+    path("products/", include("product.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
