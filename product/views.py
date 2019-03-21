9ea9341294db78edfb935cfc5acb4ba0e4bd8557
[Ryan Staley] <rericstaley@gmail.com>
2019-03-21 16:52:28 -0400

    updated styles and added search bar to product list

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Product
 
@@ -8,9 +9,23 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product/product_list.html'
     context_object_name = 'products'
+    paginate_by = 25
+
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        search = self.request.GET.get("q")
+
+        if search:
+            return queryset.filter(
+                Q(manufacturer__name__icontains=search) |
+                Q(description__icontains=search)  |
+                Q(model_number__icontains=search)
+            )
+        return queryset
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product/product_detail.html'
     context_object_name = 'product'
+
