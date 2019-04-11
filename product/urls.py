213f05968ebed949aca83e5895c4282989539a4b
Ryan Staley <skaaldig@gmail.com>
2019-04-11 14:05:26 -0400

    url for redirect view added

--- product/urls.py
+++ product/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('return/<int:pk>/', views.ProductInstanceReturnView.as_view(), name="product-return"),
+    path('redirect/<int:pk>/', views.ProductRedirectView.as_view(), name="return-redirect"),
     path('borrow/<int:pk>/', views.ProductInstanceBorrowView.as_view(), name="product-borrow"),
     path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name="product-edit"),
     path('add/', views.ProductCreateView.as_view(), name="product-add"),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name="product-detail"),
     path('all/', views.ProductListView.as_view(), name="all-products"),
 ]
