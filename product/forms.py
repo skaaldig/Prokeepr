b7cf234d02083c2d3422f35345f4c0d53715e526
Ryan Staley <rericstaley@gmail.com>
2019-05-02 15:44:18 -0400

    removed unnecessary widget customization

--- product/forms.py
+++ product/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Select
+from django.forms import ModelForm
 
 from .models import ProductInstance, Product
 
@@ -7,16 +7,10 @@ class ReturnProductForm(ModelForm):
     class Meta:
         model = ProductInstance
         fields = ('returned_to', 'return_note')
-        widgets = {
-            'returned_to': Select(attrs={'class': 'borrow__returned-to'})
-        }
 
 
 class CreateProductForm(ModelForm):
     class Meta:
         model = Product
         exclude = ('current_borrower', 'loan_status', 'created')
-        widgets = {
-            'manufacturer': Select(attrs={'class': 'create__dropdown'}),
-            'warehouse': Select(attrs={'class': 'create__dropdown'})
-        }
+
