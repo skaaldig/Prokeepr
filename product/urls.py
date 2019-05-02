0bccec253fb4ed23b7b4233b0ebbade67d3ef3db
Ryan Staley <rericstaley@gmail.com>
2019-05-02 15:55:10 -0400

    ordering and delete view added

--- product/urls.py
+++ product/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update/<int:pk>/', views.ProductUpdate.as_view(), name='product-update'),
+    path('all/', views.ProductList.as_view(), name="all-products"),
+    path('borrowed/', views.BorrowedProductList.as_view(), name="borrowed"),
     path('create/', views.ProductCreate.as_view(), name='product-create'),
     path('return/<int:pk>/', views.ProductInstanceReturn.as_view(), name="product-return"),
     path('redirect/<int:pk>/', views.ProductInstanceRedirect.as_view(), name="return-redirect"),
     path('borrow/<int:pk>/', views.ProductInstanceBorrow.as_view(), name="product-borrow"),
-    path('borrowed/', views.BorrowedProductList.as_view(), name="borrowed"),
-    path('all/', views.ProductList.as_view(), name="all-products"),
+    path('delete/<int:pk>/', views.ProductDelete.as_view(), name='product-delete'),
+    path('update/<int:pk>/', views.ProductUpdate.as_view(), name='product-update'),
 ]
